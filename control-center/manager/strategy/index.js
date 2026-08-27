ae1017042c5ff3b8ffb5cb250dd9572a1ca4fb7d
banupham <135731801+banupham@users.noreply.github.com>
2026-08-27 12:35:32 +0700

    feat(strategy): export learned recovery policy

--- control-center/manager/strategy/index.js
+++ control-center/manager/strategy/index.js
@@ -48,5 +48,6 @@ module.exports = {
   ...require('./execution_behavior_contract'),
   ...require('./offline_baseline_provider'),
   ...require('./self_experience_memory'),
-  ...require('./self_exploration_provider')
+  ...require('./self_exploration_provider'),
+  ...require('./recovery_policy_memory')
 };
